02df7066f9b49a5ad2ca1349e44c20d3e0016784
Ashish Arya <62978073+Ashish-Arya-CS@users.noreply.github.com>
2021-01-27 17:13:50 +0530

    Update CovDetec.py

--- CovDetec.py
+++ CovDetec.py
@@ -44,7 +44,7 @@ local_css("style.css")
  
 st.markdown(" <h1 style='text-align: center; color: black;'><span class='highlight slateblue'>Corona Detection App</span></h1>", unsafe_allow_html=True)
 st.markdown("\n")
-st.markdown(" <h4 style='text-align: center; color: black;'><span class='highlight slateblue'>To Know about the working of App and to Display Wave Plot, please click</span></h4>", unsafe_allow_html=True)
+st.markdown(" <div><span class='highlight slateblue'>To Know about the working of App and to Display Wave Plot, please click</span></div>", unsafe_allow_html=True)
 st.markdown(" <h4 style='text-align: center; color: black;'><span class='highlight slateblue'>on Expand to show option Button below.</span></h4>", unsafe_allow_html=True)
 
 
@@ -60,7 +60,7 @@ if 'How does it work ?' in choice:
     st.markdown("<div><span class='highlight blue'>• Once your result is displayed and you want to obtain a prediction for any other audio file then</span></div>", unsafe_allow_html=True)
     st.markdown("<div><span class='highlight blue'> it is recommended to reload the page. </span></div>", unsafe_allow_html=True)
     st.markdown("<div><span class='highlight blue'>• At last, we wish you to stay healthy and Covid Negative. Don't forget to wear Mask and</span></div>", unsafe_allow_html=True)
-    st.markdown("<div><span class='highlight blue'> maintain Social Distancing.</span><div>", unsafe_allow_html=True)
+    st.markdown("<div><span class='highlight blue'> maintain Social Distancing.</span></div>", unsafe_allow_html=True)
     st.markdown("\n")
 st.markdown(" <h3 style='text-align: center; color: black;'><span class='highlight slateblue'>Upload Your Audio File Below</span></h3>", unsafe_allow_html=True)
 st.markdown(" <h5 style='text-align: center; color: black;'><span class='highlight slateblue'>File containing cough sound should be about three seconds.</span></h5>", unsafe_allow_html=True)
